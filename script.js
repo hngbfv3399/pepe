e8050a0044f0150ba553b0f26e1a434632b16528
hngbfv3399 <hngbfv3399@gmail.com>
2023-01-28 22:59:38 +0900

    페페

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function pepe(){
-    let fuck = document.querySelector(".LRn4mf")
-    fuck.innerHTML = " "
-setInterval(() => {
-    let imgs = document.querySelectorAll('img')
+    setInterval(() => {
+    let imgs = document.querySelectorAll('img') 
     let s = document.querySelectorAll('.cS4Vcb-pGL6qe-mji9Ze')
     let fuck = document.querySelectorAll(".LRn4mf")
+    let f = document.querySelectorAll(".VlHyHc")
     imgs.forEach((a,i)=>{
         a.src = 'https://images.chosun.com/resizer/u9nJRxs0BbtjygJ4HzKukecXnOk=/464x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/UVBJZL3RXAB36BDSHVM3MW2WNY.jpg'
 
@@ -13,7 +12,10 @@ setInterval(() => {
         a.innerHTML = "여기는 페페가 지배했다"
     })
     fuck.forEach((a,i)=>{
-        a.innerHTML = "구글 초등학생 한테 털렸죠?ㅋㅋ"
+        a.innerHTML = "여기는 페페가 지배했다."
+    })
+    f.forEach((a,i)=>{
+        a.innerHTML = "여기는 페페가 지배했다."
     })
     
 }, 500);
