e5bc3c0b6905d1cae81bd7c62badd28e7227f46a
hngbfv3399 <hngbfv3399@gmail.com>
2023-01-28 22:46:21 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function pepe(){
+    let fuck = document.querySelector(".LRn4mf")
+    fuck.innerHTML = " "
+setInterval(() => {
+    let imgs = document.querySelectorAll('img')
+    let s = document.querySelectorAll('.cS4Vcb-pGL6qe-mji9Ze')
+    let fuck = document.querySelectorAll(".LRn4mf")
+    imgs.forEach((a,i)=>{
+        a.src = 'https://images.chosun.com/resizer/u9nJRxs0BbtjygJ4HzKukecXnOk=/464x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/UVBJZL3RXAB36BDSHVM3MW2WNY.jpg'
+
+    })
+    s.forEach((a,i)=>{
+        a.innerHTML = "여기는 페페가 지배했다"
+    })
+    fuck.forEach((a,i)=>{
+        a.innerHTML = "구글 초등학생 한테 털렸죠?ㅋㅋ"
+    })
+    
+}, 500);
+}
+//class="LRn4mf tmS4cc"
